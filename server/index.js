5f37b951c19cd17020a369adc97f579182de92ea
youssef essam <essamy@688gmail.com>
2026-05-07 21:15:04 +0300

    update 9 commit

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const crypto = require("crypto");
+
+if (!global.crypto) {
+    global.crypto = crypto;
+}
 
 const connectDB = require("./config/db");
 const todoRoutes = require("./routes/todos");
